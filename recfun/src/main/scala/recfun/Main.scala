ad82c427f7115abad3bd6b65c9c02b3fc5884b9c
Grzegorz Kazior <gkazior@interia.pl>
2013-04-03 17:01:02 +0200

    work revision: Pascal and balance works fine

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,6 @@
 package recfun
-import common._
+
+import scala.annotation.tailrec
 
 object Main {
   def main(args: Array[String]) {
@@ -13,16 +14,48 @@ object Main {
 
   /**
    * Exercise 1
+   * Sample pascal triangle
+   *       1      // r = 0 c = 0
+   *      1 1     // r = 1 c = 0,1
+   *     1 2 1    // r = 2 c = 0,1,2
+   *    1 3 3 1   // r = 3 c = 0,1,2,3
+   *   1 4 6 4 1  // r = 4 c = 0,1,2,3,4
+   *
+   * Columns and rows indexed from 0
+   *
+   * Function is not optimized. Stack usage is not optimized.
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c < 0 || r < 0) throw new java.util.NoSuchElementException
+
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
 
   /**
    * Exercise 2
+   *
+   * See the test suite for
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec
+    def balanceHelper(currentlyOpenNo: Int, chars: List[Char]): Boolean = {
+      if (currentlyOpenNo < 0) false
+      else if (chars.isEmpty) currentlyOpenNo == 0
+      else chars.head match {
+        case '(' => balanceHelper(currentlyOpenNo + 1, chars.tail)
+        case ')' => balanceHelper(currentlyOpenNo - 1, chars.tail)
+        case _ => balanceHelper(currentlyOpenNo, chars.tail)
+      }
+
+    }
+    balanceHelper(0, chars);
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    0
+  }
 }
--- recfun/src/test/scala/recfun/PascalSuite.scala
+++ recfun/src/test/scala/recfun/PascalSuite.scala
@@ -8,15 +8,59 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class PascalSuite extends FunSuite {
   import Main.pascal
+  test("pascal: invalid c -1") {
+    intercept[NoSuchElementException] {
+      pascal(-1, 2)
+    }
+  }
+  test("pascal: invalid c -5") {
+    intercept[NoSuchElementException] {
+      pascal(-5, 2)
+    }
+  }
+  test("pascal: invalid r -1") {
+    intercept[NoSuchElementException] {
+      pascal(0, -1)
+    }
+  }
+  test("pascal: invalid r -5") {
+    intercept[NoSuchElementException] {
+      pascal(10, -5)
+    }
+  }
+  test("pascal: invalid c and r") {
+    intercept[NoSuchElementException] {
+      pascal(-1, -1)
+    }
+  }
   test("pascal: col=0,row=2") {
-    assert(pascal(0,2) === 1)
+    assert(pascal(0, 2) === 1)
   }
-
   test("pascal: col=1,row=2") {
-    assert(pascal(1,2) === 2)
+    assert(pascal(1, 2) === 2)
+  }
+  test("pascal: col=1,row=3") {
+    assert(pascal(1, 3) === 3)
+  }
+  test("pascal: tree") {
+    val expectedResults = List(List(1) //
+    , List(1, 1) //
+    , List(1, 2, 1) //
+    , List(1, 3, 3, 1) //
+    , List(1, 4, 6, 4, 1) //
+    , List(1, 5, 10, 10, 5, 1) //
+    )
+
+    var idxRow = 0
+    for (rowList <- expectedResults) {
+      var idxColumn = 0
+      for (value <- rowList) {
+        assert(pascal(idxColumn, idxRow) === value)
+        idxColumn = idxColumn + 1
+      }
+      idxRow = idxRow + 1
+    }
+
   }
 
-  test("pascal: col=1,row=3") {
-    assert(pascal(1,3) === 3)
-  }
 }
