c8c1e049d89c6f4a82815de6c5e2e0d50aadfbd3
Grzegorz Kazior <gkazior@interia.pl>
2013-04-03 23:00:07 +0200

    work revision: balance works nice, need to read about scala test (see the TODO task), countChange is missing

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -22,6 +22,8 @@ object Main {
    *   1 4 6 4 1  // r = 4 c = 0,1,2,3,4
    *
    * Columns and rows indexed from 0
+   * Returns the value from the pascal triangle indexed by (c,r).
+   * Ex. pascal(1,2) gets the item from the first column and second row (counted from 0)
    *
    * Function is not optimized. Stack usage is not optimized.
    */
@@ -32,24 +34,42 @@ object Main {
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
+  @tailrec
+  private def balanceHelper(currentlyOpenNo: Int, chars: List[Char]): Boolean = {
+    if (currentlyOpenNo < 0) false
+    else if (chars.isEmpty) currentlyOpenNo == 0
+    else chars.head match {
+      case '(' => balanceHelper(currentlyOpenNo + 1, chars.tail)
+      case ')' => balanceHelper(currentlyOpenNo - 1, chars.tail)
+      case _ => balanceHelper(currentlyOpenNo, chars.tail)
+    }
+  }
+  @tailrec
+  private def balance2Helper(currentlyOpenNo: Int, chars: List[Char]): Boolean = {
+    if (currentlyOpenNo < 0) false
+    else chars match {
+      case Nil => currentlyOpenNo == 0
+      case '(' :: tail => balance2Helper(currentlyOpenNo + 1, tail)
+      case ')' :: tail => balance2Helper(currentlyOpenNo - 1, tail)
+      case _ :: tail => balance2Helper(currentlyOpenNo, tail)
+    }
+  }
+
   /**
    * Exercise 2
    *
    * See the test suite for
+   *
    */
   def balance(chars: List[Char]): Boolean = {
-    @tailrec
-    def balanceHelper(currentlyOpenNo: Int, chars: List[Char]): Boolean = {
-      if (currentlyOpenNo < 0) false
-      else if (chars.isEmpty) currentlyOpenNo == 0
-      else chars.head match {
-        case '(' => balanceHelper(currentlyOpenNo + 1, chars.tail)
-        case ')' => balanceHelper(currentlyOpenNo - 1, chars.tail)
-        case _ => balanceHelper(currentlyOpenNo, chars.tail)
-      }
-
-    }
     balanceHelper(0, chars);
+  }
+
+  /* 
+   * Should be the same as balance2. I only use pattern matching which is great in scala.
+   * */
+  def balance2(chars: List[Char]): Boolean = {
+    balance2Helper(0, chars);
   }
 
   /**
--- recfun/src/test/scala/recfun/BalanceSuite.scala
+++ recfun/src/test/scala/recfun/BalanceSuite.scala
@@ -1,14 +1,26 @@
 package recfun
 
-import org.scalatest.FunSuite
-
 import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import Main.balance
+import Main.balance2
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class BalanceSuite extends FunSuite {
-  import Main.balance
-
+  def testBalanceFn(testName: String, balanceFn: (List[Char]) => Boolean, arg: String, expectedResult: Boolean): Unit = {
+    test(testName) {
+      assert(balanceFn(arg.toList) === expectedResult)
+    }
+  }
+  def testHelper(expectedResult: Boolean, arg: String): Unit = {
+    // Very funny way to make the unique name for testcase
+    // TODO: read scala-test guide
+    // Names proposed as embedded names are not nice too ex. first name is cut just after first space
+    testBalanceFn("balance : " + arg.hashCode, balance, arg, expectedResult)
+    testBalanceFn("balance2: " + arg.hashCode, balance2, arg, expectedResult)
+  }
+  // I do not change embedded tests - in fact I am curious of the mechanics behind assignment
   test("balance: '(if (zero? x) max (/ 1 x))' is balanced") {
     assert(balance("(if (zero? x) max (/ 1 x))".toList))
   }
@@ -24,4 +36,49 @@ class BalanceSuite extends FunSuite {
   test("balance: counting is not enough") {
     assert(!balance("())(".toList))
   }
+
+  // Bad idea. When test fails the stack does not show the problem. Better to use arg.hashCode ;-)
+  test("True values") {
+    for (
+      arg <- List( //
+        "(if (zero? x) max (/ 1 x))' is balanced" //
+        , "I told him (that it's not (yet) done).\n(But he wasn't listening)" //
+        , "()" //
+        , "" //
+        , "This works as well" //
+        , "\n\n", "(\n\n)", "(\n(\n))" //
+        , "()()()()", "(((((((())))))))" //
+        )
+    ) {
+      assert(balance(arg.toList))
+      assert(balance2(arg.toList))
+    }
+  }
+
+  testHelper(false, ")")
+  testHelper(false, "(")
+  testHelper(false, "))")
+  testHelper(false, "((")
+  testHelper(false, ")(")
+  testHelper(false, "(()")
+  testHelper(false, "(()))")
+  testHelper(false, ":-)")
+  testHelper(false, ":-(")
+  testHelper(false, "())(")
+
+  // As "True values". That code does not lead to good problem diagnosis
+  test("False values") {
+    for (
+      arg <- List( //
+        ")", "(", "))", "((", ")(" //
+        , "(()", "())" //
+        , "())(" //
+        , ":-)", ":-(" // Secret weapon
+        )
+    ) {
+      assert(!balance(arg.toList))
+      assert(!balance2(arg.toList))
+    }
+  }
+
 }
